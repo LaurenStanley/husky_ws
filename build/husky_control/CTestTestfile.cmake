069780feed28e320e509e83a77637faf54bbeb03
Lauren <lestanley48@gmail.com>
2022-07-18 13:25:52 -0400

    remove build folder, update realsense stl

--- build/husky_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lauren/husky_ws/src/husky/husky_control
-# Build directory: /home/lauren/husky_ws/build/husky_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
