7048c472b763acb4a1b14f005ac292c36e7384a9
Lauren <lestanley48@gmail.com>
2022-07-19 12:55:10 -0400

    Modified rviz robot launch file

--- install/husky_viz/share/husky_viz/launch/view_robot.launch.py
+++ install/husky_viz/share/husky_viz/launch/view_robot.launch.py
@@ -4,16 +4,24 @@ import sys
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
+import launch
 
 
 def generate_launch_description():
 
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("husky_viz"), "rviz", "test.rviz"]
+    )
+    
+    
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
     )
 
     node_rviz = Node(
@@ -26,6 +34,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            joint_state_publisher_node,
             node_rviz,
         ]
     )
