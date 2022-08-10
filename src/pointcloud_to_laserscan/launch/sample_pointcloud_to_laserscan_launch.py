d974d09d21680b77f926ac32895255f7cb07c6e1
Lauren <lestanley48@gmail.com>
2022-08-10 14:21:42 -0400

    Global Map settings included!
    If no obstacle detected the map range extends. Change inf_epsilon in laserscan to pointcloud launch file (thanks dev!)

--- src/pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
+++ src/pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
@@ -17,16 +17,16 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': 'velodyne',
                 'transform_tolerance': 0.01,
-                'min_height': -1.0,
+                'min_height': -0.75,
                 'max_height': 10.0,
                 'angle_min': -3.1415,
                 'angle_max': 3.1415,
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.3,
+                'range_min': 0.9,
                 'range_max': 120.0,
-                'use_inf': True,
-                'inf_epsilon': 1.0,
+                'use_inf': False,
+                'inf_epsilon': -1.0,
                 'use_sim_time': True,
             }],
             name='pointcloud_to_laserscan'
