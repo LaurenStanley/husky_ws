b037ad8f5258cfeeffdc44ab38ee58d87e70807d
Lauren <lestanley48@gmail.com>
2022-10-19 16:49:15 -0400

    add husky foxy workspace

--- src/nav2_tutorial/scripts/nav_to_pose.py
+++ src/nav2_tutorial/scripts/nav_to_pose.py
@@ -19,8 +19,8 @@ class GoalPosePublisherNode(Node):
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp.sec = 0
-        goal_pose.pose.position.x = 4.0
-        goal_pose.pose.position.y = 0.0
+        goal_pose.pose.position.x = 14.0
+        goal_pose.pose.position.y = 5.0
         goal_pose.pose.position.z = 0.0
         goal_pose.pose.orientation.x = 0.0
         goal_pose.pose.orientation.y = 0.0
